0f568a0b7f829d2d6a4e205d1364a74753a22b75
Robin Blixter <robin.blixter@intersport.se>
2020-08-27 21:58:06 +0200

    Done with Section 18

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Profiles;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,12 @@ namespace API.Controllers
         public async Task<ActionResult<Profile>> Get(string username)
         {
             return await Mediator.Send(new Details.Query{Username = username});
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Unit>> Edit(Edit.Command command)
+        {
+            return await Mediator.Send(command);
         }
     }
 }
